3e10dd03157e97ad56a34520b32d66a140e81b45
AlinaBielska <alinkabelka5@gmail.com>
2023-03-17 21:34:54 +0100

    zadanie 1 - done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,20 @@
 const buttonStart = document.querySelector("[data-start]");
 const buttonStop = document.querySelector("[data-stop]");
+const bodyColor = document.querySelector("body");
 let timerId = null;
 
 buttonStart.addEventListener("click", () => {
     timerId = setInterval(() => {
-       color.style.backgroundColor = getRandomHexColor();
+       bodyColor.style.backgroundColor = getRandomHexColor();
     }, 1000);
+    buttonStart.setAttribute('disabled', '');
 });
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
+};
+
+buttonStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    buttonStart.removeAttribute('disabled');
+});
