eb0bfe9999519912e975206c0013966e63c72ea9
AlinaBielska <alinkabelka5@gmail.com>
2023-03-17 21:16:02 +0100

    zadania 1 - początek

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const buttonStart = document.querySelector("[data-start]");
+const buttonStop = document.querySelector("[data-stop]");
+let timerId = null;
 
+buttonStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+       color.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
