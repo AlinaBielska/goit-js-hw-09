783ec8d2723df8861897db3faa8214b0b948e58d
AlinaBielska <113899336+AlinaBielska@users.noreply.github.com>
2023-03-17 20:16:29 +0000

    Deploying to gh-pages from @ AlinaBielska/goit-js-hw-09@eb0bfe9999519912e975206c0013966e63c72ea9 🚀

---
+++ 01-color-switcher.024ab853.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(function(){setInterval((function(){color.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.024ab853.js.map
---
+++ 01-color-switcher.f40874b7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");let e=null;t.addEventListener("click",(()=>{e=setInterval((()=>{color.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)}));
+//# sourceMappingURL=01-color-switcher.f40874b7.js.map
