d255bfd27083485eefab18a0f3d287038bfa7086
AlinaBielska <113899336+AlinaBielska@users.noreply.github.com>
2023-03-17 20:35:21 +0000

    Deploying to gh-pages from @ AlinaBielska/goit-js-hw-09@3e10dd03157e97ad56a34520b32d66a140e81b45 🚀

---
+++ 01-color-switcher.16e134d3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(()=>{clearInterval(a),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.16e134d3.js.map
