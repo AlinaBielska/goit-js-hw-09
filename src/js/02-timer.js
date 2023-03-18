a4ddb739cd630f662c3a9cd4e2d461e5928cebe3
AlinaBielska <alinkabelka5@gmail.com>
2023-03-18 21:39:50 +0100

    zadanie 2 - start

---
+++ src/js/02-timer.js
@@ -0,0 +1,43 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const button = document.querySelector("[data-start]");
+
+const fp = flatpickr("#datetime-picker", {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      let dateNow = new Date();
+      if (selectedDates[0].getTime() < dateNow.getTime()) {
+          window.alert("Please choose a date in the future");
+          button.setAttribute('disabled', '');
+      } else {
+          button.removeAttribute('disabled');
+      }
+    },
+});
+
+button.addEventListener("click", convertMs(ms));
+let ms = 
+  
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
