92768c3df09800b93373c98377c3056dfd6a4f49
AlinaBielska <alinkabelka5@gmail.com>
2023-03-19 15:16:46 +0100

    interfejs reaguje na zmianę pozostałego czasu

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,12 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const button = document.querySelector("[data-start]");
 let selectedDate = 0;
+const span = {
+  day: document.querySelector("[data-days]"),
+  hour: document.querySelector("[data-hours]"),
+  minute: document.querySelector("[data-minutes]"),
+  second: document.querySelector("[data-seconds]"),
+};
 
 const fp = flatpickr("#datetime-picker", {
   enableTime: true,
@@ -23,10 +29,17 @@ const fp = flatpickr("#datetime-picker", {
 });
 
 button.addEventListener("click", () => {
+  let timerId = setInterval(() => {
   let ms = new Date(selectedDate).getTime() - new Date().getTime();
-  const timerData = convertMs(ms)
-  console.log(timerData);
-});
+  const timerData = convertMs(ms);
+  // console.log(timerData);
+  span.second.textContent = timerData.seconds;
+  span.minute.textContent = timerData.minutes;
+  span.hour.textContent = timerData.hours;
+  span.day.textContent = timerData.days;
+  }, 1000);
+}
+);
   
 function convertMs(ms) {
   // Number of milliseconds per unit of time
