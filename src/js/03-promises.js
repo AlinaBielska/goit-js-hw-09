65b60cb732383252141ce5530a01a898a11a1d70
AlinaBielska <alinkabelka5@gmail.com>
2023-03-22 20:38:42 +0100

    zadanie 3 - done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+const { delay, step, amount } = form.elements;
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const eventPromise = e => {
+  e.preventDefault();
+  let generalDelay = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, generalDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    
+    generalDelay += Number(step.value);
+  }
+};
+
+form.addEventListener("submit", eventPromise);
